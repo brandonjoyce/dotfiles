ff118d8806e3c5ba8ad8d3f44b1e24ef37e2bd58
Brandon Joyce <bjoyce@covermymeds.com>
2016-01-01 22:06:42 -0500

    Override existing files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-stow bash
-stow git
-stow tmux
-stow tmuxinator
-stow vim
-stow zsh
+stow bash override=bash
+stow git override=git
+stow tmux override=tmux
+stow tmuxinator override=tmuxinator
+stow vim override=vim
+stow zsh override=zsh
