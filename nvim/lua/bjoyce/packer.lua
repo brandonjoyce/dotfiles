98088cf8a80635d551287008ba7488808ed9c6f5
Brandon Joyce <bjoyce@bitfreighter.com>
2024-01-20 15:56:42 -0500

    Adding remaps

--- nvim/lua/bjoyce/packer.lua
+++ nvim/lua/bjoyce/packer.lua
@@ -27,4 +27,23 @@ return require('packer').startup(function(use)
 
 	use { "alexghergh/nvim-tmux-navigation" }
 	use('tpope/vim-fugitive')
+
+	-- Language server and auto-complete stuff
+	use { 
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lua'},
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'}
+		}
+	}
+
 end)
--- nvim/lua/bjoyce/remap.lua
+++ nvim/lua/bjoyce/remap.lua
@@ -14,3 +14,42 @@ vim.keymap.set("n", "<leader>q", ":q<Enter>")
 -- Previous Buffer
 vim.keymap.set("n", "<leader><leader>", "<C-^>")
 
+-- Move highlighted
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor position when J'ing
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor centered when using C-d/C-u
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-d>", "<C-u>zz")
+
+-- Keep cursor centered when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste over without losing current paste buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Delete without losing current paste buffer
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Quickfix shortcuts
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace current word in file
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
