cf484fe310eaa195084781e69f975b105794f087
Brandon Joyce <brandon@sonerdy.com>
2024-01-24 22:36:09 -0500

    Adding language servers

--- astrovim_config/plugins/treesitter.lua
+++ astrovim_config/plugins/treesitter.lua
@@ -3,8 +3,21 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+      "bash",
+      "css",
+      "dockerfile",
+      "elixir",
+      "erlang",
+      "html",
+      "javascript",
+      "json",
       "lua",
-      "elixir"
+      "markdown",
+      "python",
+      "ruby",
+      "terraform",
+      "typescript",
+      "xml"
     })
   end,
 }
