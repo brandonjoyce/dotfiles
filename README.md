2f889dd36bb10352b9ac1ce2ac2fd653bf4b2d55
Brandon Joyce <bjoyce@bitfreighter.com>
2024-04-12 20:00:04 -0400

    Add shortcut for buffers tree

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-dotfiles
-========
+# dotfiles
 
 These are my dotfiles which I manage with Stow.  
 Here is a nice post on how it's done. http://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html
 
-Things to Install
-=================
+# Things to Install
 
 - iTerm
 - OhMyZSH
-- tmux/tmate
+- tmux
 - Docker for Mac
--- astronvim_v4/lua/plugins/astrocore.lua
+++ astronvim_v4/lua/plugins/astrocore.lua
@@ -61,6 +61,7 @@ return {
           end,
           desc = "Pick to close",
         },
+        ["<leader>be"] = { ":Neotree buffers<Enter>", desc = "Buffers treeview" },
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         ["<Leader>b"] = { desc = "Buffers" },
